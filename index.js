906f58195988f1e98279e99edce642352d695c93
helicopterchen <helicopter@mail.com>
2019-05-16 17:16:27 +0800

    1

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ let PORT = 19999
 
 let app = express();
 app.use(express.static("./public"))
-if(!fs.existsSync("./saved_data")){
-    fs.mkdirSync("./saved_data");
-}
 
 app.get("/", async(req, res)=> {
     fs.readFile("./index.html",(err,data)=>{
